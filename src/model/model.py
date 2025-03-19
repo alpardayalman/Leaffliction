a24b4f94d18c7272355a4ea725308f700744fd15
timurci <timurcakmakoglu@gmail.com>
2025-03-19 12:27:30 +0300

    Updated dataset loader

--- src/model/model.py
+++ src/model/model.py
@@ -3,25 +3,30 @@ import pandas as pd
 import cv2 as cv
 import torch
 from torch.utils.data import Dataset
+from sklearn.preprocessing import LabelEncoder
 
 
 class CustomImageDataset(Dataset):
     def __init__(self, parent_dir, transform=None, target_transform=None):
-        self.info = self.annotate(parent_dir)
+        images, labels, label_encoder = self.annotate(parent_dir)
+        self.images = images
+        self.labels = labels
+        self.label_encoder = label_encoder
         self.transform = transform
         self.target_transform = target_transform
 
     def __len__(self):
-        return len(self.info)
+        return len(self.labels)
 
     def __getitem__(self, idx):
-        img_info = self.info.iloc[idx]
-        path = os.path.join(img_info["dir"], img_info["file"])
+        img = self.images.iloc[idx]
+        path = os.path.join(img["dir"], img["file"])
 
         img = cv.imread(path)
         img = cv.cvtColor(img, cv.COLOR_BGR2RGB)
-        img = torch.from_numpy(img)
-        label = img_info["label"]
+        # img = torch.from_numpy(img).type(torch.float32)
+
+        label = self.labels[idx]
 
         if self.transform:
             img = self.transform(img)
@@ -41,11 +46,13 @@ class CustomImageDataset(Dataset):
                 filenames.append(f)
                 labels.append(os.path.basename(dirname))
 
-        annotation = pd.DataFrame({"dir": dirnames,
-                                   "file": filenames,
-                                   "label": labels})
+        label_encoder = LabelEncoder()
+        labels = label_encoder.fit_transform(labels)
+        labels = torch.from_numpy(labels)
+        
+        images = pd.DataFrame({"dir": dirnames,
+                               "file": filenames})
 
-        annotation["dir"] = annotation["dir"].astype("category")
-        annotation["label"] = annotation["label"].astype("category")
+        images["dir"] = images["dir"].astype("category")
 
-        return annotation
+        return images, labels, label_encoder
